c9029c0d5c1acb5f76001874c8808050853fa2a6
Bombbom <doctor12221@gmail.com>
2022-07-20 22:36:10 -0300

    V:0.20.0:correction

--- models/miyu.js
+++ models/miyu.js
@@ -1,9 +1,9 @@
 //require('../dotenv/config');	
 const Sequelize=require('sequelize')
 //just development
-const sequelize= new Sequelize('miyu', 'root', 'miyudatabase', {host: 'localhost', dialect: 'mysql'})
+//const sequelize= new Sequelize('miyu', 'root', 'miyudatabase', {host: 'localhost', dialect: 'mysql'})
 //just online
-//const sequelize= new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {host: process.env.DB_HOST, dialect: 'mysql'})
+const sequelize= new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {host: process.env.DB_HOST, dialect: 'mysql'})
 sequelize.authenticate().then(function(){console.log('conectado')}).catch(function(erro){console.log('nao conectado: '+erro)})
 sequelize.sync({alter: true})
 module.exports=sequelize;
